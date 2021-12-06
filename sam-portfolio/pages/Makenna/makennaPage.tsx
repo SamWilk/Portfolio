794b6bb5865916584f0ddfef448525359ae2da65
SamWilk <samwilk18@yahoo.com>
2021-12-05 19:09:19 -0500

    Styled and updated Home page

---
+++ sam-portfolio/pages/Makenna/makennaPage.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const makennaPage = () => {
+  return (
+    <>
+      <div>
+        <Image src="/MakennaPic.jpeg" width={400} height={400} layout="fixed" />
+      </div>
+    </>
+  );
+};
+
+export default makennaPage;
