3f982ebd3555d66e37dc0150ce5f707ed6198b31
SamWilk <samwilk18@yahoo.com>
2021-12-05 19:32:04 -0500

    Starting styling and functionality of About me page

--- sam-portfolio/pages/About-me/Aboutme.tsx
+++ sam-portfolio/pages/About-me/Aboutme.tsx
@@ -1,7 +1,31 @@
 import styles from "../About-me/Aboutme.module.css";
+import Link from "next/link";
 
 const Aboutme = () => {
-  return <div className={styles.container}>About me...</div>;
+  function showEducation() {
+    let list = "words go here";
+    document.getElementById("text")!.innerHTML = list;
+  }
+  function showWork() {
+    let list = "Words were here";
+    document.getElementById("text")!.innerHTML = list;
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.list}>
+          <Link href="/About-me/Aboutme">
+            <a onClick={() => showEducation()}>Education</a>
+          </Link>
+          <Link href="/About-me/Aboutme">
+            <a onClick={() => showWork()}>Work</a>
+          </Link>
+        </div>
+        <div id="text"></div>
+      </div>
+    </>
+  );
 };
 
 export default Aboutme;
