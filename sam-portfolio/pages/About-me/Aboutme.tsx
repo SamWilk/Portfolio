0aaa89168d201b4791f250c128856ecf795ba6e0
SamWilk <samwilk18@yahoo.com>
2021-12-06 18:34:47 -0500

    Changing the fonts

--- sam-portfolio/pages/About-me/Aboutme.tsx
+++ sam-portfolio/pages/About-me/Aboutme.tsx
@@ -1,7 +1,18 @@
 import styles from "../About-me/Aboutme.module.css";
+import Head from "next/head";
 
 const Aboutme = () => {
-  return <div className={styles.container}>About me...</div>;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className={styles.container}>About me...</div>
+    </>
+  );
 };
 
 export default Aboutme;
--- sam-portfolio/pages/Projects/projects.tsx
+++ sam-portfolio/pages/Projects/projects.tsx
@@ -1,8 +1,15 @@
 import styles from "../Projects/projects.module.css";
+import Head from "next/head";
 
 const searchingList = () => {
   return (
     <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <div className={styles.container}>Projects....</div>
     </>
   );
