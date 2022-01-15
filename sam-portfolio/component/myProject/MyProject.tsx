fa2ccd0126e198a37e27a17d9fb3ff204a7a1a4f
SamWilk <samwilk18@yahoo.com>
2022-01-15 17:10:10 -0500

    Push

--- sam-portfolio/component/myProject/MyProject.tsx
+++ sam-portfolio/component/myProject/MyProject.tsx
@@ -1,10 +1,21 @@
 import styles from "../myProject/MyProject.module.css";
 
-const MyProject = () => {
+const MyProject = (prop: any) => {
+  const {
+    props: { projects },
+  } = prop;
   return (
     <>
       <div className={styles.container}>
-        <div>Projects...</div>
+        <div className={styles.content}>
+          {projects.map((items: any) => (
+            <div key={items.id}>
+              <div key={items.id}>{items.name}</div>
+              <div key={items.id}>fdafdsa</div>
+            </div>
+          ))}
+        </div>
+        <pre>{JSON.stringify(projects, 0, 2)}</pre>
       </div>
     </>
   );
